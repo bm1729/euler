cf0e2f11f1c6a23b805996b5b9499bdce884e965
ben.mumford <ben.mumford1729@gmail.com>
2018-10-11 15:39:14 +0100

    Macro for timing

--- test/euler/core_test.clj
+++ test/euler/core_test.clj
@@ -14,19 +14,25 @@
              [problem-011 :refer :all]
              [problem-012 :refer :all]]))
 
-(println "Hello")
+(defmacro euler
+  [expected solution-fn]
+  `(deftest ~(symbol (str "testing-" solution-fn))
+     (let [start-ns# (. java.lang.System (nanoTime))]
+       (is (= ~expected (~solution-fn)))
+       (let [time-taken-ns# (- (. java.lang.System (nanoTime)) start-ns#)
+             time-taken-ms# (/ time-taken-ns# 1000000.0)]
+         (println ~(str solution-fn) time-taken-ms# "ms")
+         (is (< time-taken-ms# 5000.0))))))
 
-(deftest fast-running-solutions
-  (is (= 233168 (solution-001)))
-  (is (= 4613732 (solution-002)))
-  (is (= 6857 (solution-003)))
-  (is (= 906609 (solution-004)))
-  (is (= 232792560 (solution-005)))
-  (is (= 25164150 (solution-006)))
-  (is (= 23514624000 (solution-008)))
-  (is (= 31875000 (solution-009)))
-  (is (= 70600674 (solution-011))))
+(euler 233168 solution-001)
+(euler 4613732 solution-002)
+(euler 6857 solution-003)
+(euler 906609 solution-004)
+(euler 232792560 solution-005)
+(euler 25164150 solution-006)
+(euler 23514624000 solution-008)
+(euler 31875000 solution-009)
+(euler 70600674 solution-011)
 
-(deftest slow-running-solutions
-  (is (= 104743 (time (solution-007))))
-  (is (= 142913828922 (time (solution-010)))))
+(euler 104743 solution-007)
+(euler 142913828922 solution-010)
