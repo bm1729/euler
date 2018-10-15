c4016df24572b037973bf4e6917817a450ae95d6
ben.mumford <ben.mumford1729@gmail.com>
2018-10-15 13:30:13 +0100

    Speed up solution 12

--- src/euler/problems/problem_012.clj
+++ src/euler/problems/problem_012.clj
@@ -7,22 +7,28 @@
   (let [[s _r] (math/exact-integer-sqrt n)]
     (->> (range 1 (inc s))
          (filter (partial divides? n))
-         (mapcat (fn [x] [x (/ n x)]))
-         distinct
+         (mapcat (fn [x]
+                   (if (= n (* x x))
+                     [x]
+                     [x (/ n x)])))
          count)))
 
-(defn- triangle-factors-count
+(defn- triangular-number-factors-count
   [n]
-  (let [[a b] (if (even? n) [(/ n 2) (inc n)] [(/ (inc n) 2) n])]
+  (let [[a b] (if (even? n)
+                [(/ n 2) (inc n)]
+                [(/ (inc n) 2) n])]
     (* (factors-count a) (factors-count b))))
+
+(defn- triangular-number
+  [n]
+  (/ (* n (inc n)) 2))
 
 (defn solution-012
   []
-  (loop [n 1]
-    (let [num-factors (triangle-factors-count n)]
-      (if (< 500 num-factors)
-        (/ (* n (inc n)) 2)
-        (recur (inc n))))))
+  (some #(when (< 500 (triangular-number-factors-count %))
+           (triangular-number %))
+        (iterate inc 1)))
 
 
 
