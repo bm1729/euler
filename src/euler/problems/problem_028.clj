e14de324798d2edd43fc8b093f03811c6e4548ce
ben.mumford <ben.mumford1729@gmail.com>
2018-10-26 20:02:07 +0100

    Simplified 28

--- src/euler/problems/problem_028.clj
+++ src/euler/problems/problem_028.clj
@@ -1,13 +1,15 @@
 (ns euler.problems.problem-028
   (:require [euler.utils :refer :all]))
 
-(defn- corner-numbers
+(defn- sum-corner-numbers
   [n]
-  (map #(- (square n) (* % (- n 1))) (range 0 4)))
+  (+ (* 4 (square n))
+     (* -6 n)
+     6))
 
 (defn solution-028
   []
   (->> (range 3 1002 2)                                     ;; Dimensions of squares
-       (mapcat corner-numbers)
+       (map sum-corner-numbers)
        (reduce +)
        inc))                                                ;; Add one for the centre
