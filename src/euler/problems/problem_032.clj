cbce076fe9dfbfff5c81f7d697ff618deb5f04ca
ben.mumford <ben.mumford1729@gmail.com>
2018-11-02 07:55:14 +0000

    Removed comment

--- src/euler/problems/problem_032.clj
+++ src/euler/problems/problem_032.clj
@@ -1,16 +1,6 @@
 (ns euler.problems.problem-032
   (:require [euler.utils :refer :all]))
 
-;We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example,
-; the 5-digit number, 15234, is 1 through 5 pandigital.
-;
-;The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1
-; through 9 pandigital.
-;
-;Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
-;
-;HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
-
 (defn- is-2-3-4-pandigital-product?
   [a b c d e f g h i]
   (let [multiplicand (compose-number [a b])
